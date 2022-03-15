a1b4a444af94503cef231949f6f014322c03ba19
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-15 20:08:30 -0300

    add: tests for visualizing tools.

---
+++ tests/test_visualization.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from src import data_loading, visualization
+
+
+def test_plot_image_custom_figsize(one_image):
+    raw_image, header = one_image
+    figure, axis = visualization.plot_image(
+        data_loading._parse_raw_data(raw_image, header)[0],
+        dict(figsize=(12, 12)),
+    )
+    assert isinstance(figure, plt.Figure)
+    assert isinstance(axis, plt.Axes)
+    assert figure.get_size_inches().tolist() == [12, 12]
+
+
+def test_plot_image_default_figsize(one_image):
+    raw_image, header = one_image
+    figure, axis = visualization.plot_image(
+        data_loading._parse_raw_data(raw_image, header)[0],
+    )
+    assert figure.get_size_inches().tolist() == [10, 10]
