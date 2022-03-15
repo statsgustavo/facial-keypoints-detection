c8f33cf076e76c3147ee3bb082c0e13227c7ca22
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-15 20:08:08 -0300

    add: tools for displaying images on the streamlit app.

---
+++ src/visualization.py
@@ -0,0 +1,35 @@
+import toolz as tz
+from matplotlib import pyplot as plt
+
+from src import data_loading, helpers
+
+
+def plot_image(image, figure_kws=None):
+    if figure_kws is None:
+        figure_kws = dict(figsize=(10, 10))
+    else:
+        fig_kws = dict(figsize=(10, 10))
+        fig_kws.update(figure_kws)
+
+    figure, axis = plt.subplots(1, 1, **figure_kws)
+    axis.imshow(image)
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
+
+    return figure, axis
+
+
+def plot_key_points(image, keypoints, figure_kws=None):
+    if figure_kws is None:
+        figure_kws = dict(figsize=(10, 10))
+    else:
+        fig_kws = dict(figsize=(10, 10))
+        fig_kws.update(figure_kws)
+
+    figure, axis = plt.subplots(1, 1, **figure_kws)
+    axis.imshow(image)
+    axis.scatter(keypoints.x, keypoints.y, s=100, c="red", marker="x")
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
+
+    return figure, axis
