543d2e191e299e3f1040b6ca320ab72cf2486d05
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-16 19:24:46 -0300

    update: add fixture for testing resnet blocks.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 
+import numpy as np
 import pytest
+import tensorflow as tf
 from src import data_loading, helpers
 
 
@@ -20,3 +22,10 @@ def load_params(metadata):
 def one_image(load_params):
     image, header = next(data_loading.read_raw_images(*load_params))
     return image, header
+
+
+@pytest.fixture(scope="session")
+def input_tensor():
+    return tf.random.normal(
+        (1, 28, 28, 3), seed=42
+    )  # (bacth_size, height, width, num_channels)
