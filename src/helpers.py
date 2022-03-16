56df20addf5befb3b5937787a14f3958f1306196
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-16 19:24:16 -0300

    update: helpers for checking convolution results dimensions.

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 import hydra
+import numpy as np
 import omegaconf
 
 
@@ -8,8 +9,46 @@ def load_metadata(folders: List[str], files: List[str]) -> Dict[str, Any]:
     overides_ = [f"+{a}={b}" for a, b in zip(folders, files)]
 
     with hydra.initialize_config_module(config_module="src.conf"):
-        metadata = hydra.compose(
-            overrides=overides_
-        )
+        metadata = hydra.compose(overrides=overides_)
 
     return omegaconf.OmegaConf.to_object(metadata)
+
+
+def _compute_one_dimension_size(
+    input_dim_size: int, filter_dim_size: int, stride: int, padding: int
+) -> int:
+    return np.floor(
+        ((input_dim_size + 2 * padding - filter_dim_size) / stride) + 1
+    ).astype(np.int32)
+
+
+def _same_convolution_padding(filter_shape):
+    filter_height, filter_width = filter_shape
+    same_padding_height, same_padding_width = (
+        (filter_height - 1) / 2,
+        (filter_width - 1) / 2,
+    )
+    return same_padding_height, same_padding_width
+
+
+def convolution_output_dimension(
+    input_shape: Tuple[int],
+    filter_shape: Tuple[int],
+    stride: Tuple[int],
+    padding: Tuple[int],
+    num_filters: int,
+) -> Tuple[int]:
+    input_height, input_width = input_shape
+    filter_height, filter_width = filter_shape
+    stride_height, stride_width = stride
+    padding_height, padding_with = padding
+
+    output_height = _compute_one_dimension_size(
+        input_height, filter_height, stride_height, padding_height
+    )
+
+    output_width = _compute_one_dimension_size(
+        input_width, filter_width, stride_width, padding_with
+    )
+
+    return (output_height, output_width, num_filters)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,3 +1,5 @@
+import hypothesis as hp
+from hypothesis import strategies as st
 from src import helpers
 
 
@@ -5,8 +7,38 @@ def test_load_metadata():
     metadata = helpers.load_metadata(["data"], ["raw"])
     assert isinstance(metadata, dict)
     assert "data" in metadata
-    assert "training" in  metadata["data"]
-    assert "test" in  metadata["data"]    
-    assert "delimiter" in  metadata["data"]
+    assert "training" in metadata["data"]
+    assert "test" in metadata["data"]
+    assert "delimiter" in metadata["data"]
 
 
+def test_convolution_output_dimension():
+    assert (4, 4, 5) == helpers.convolution_output_dimension(
+        (6, 6), (3, 3), (1, 1), (0, 0), 5
+    )
+    assert (2, 2, 5) == helpers.convolution_output_dimension(
+        (6, 6), (3, 3), (2, 2), (0, 0), 5
+    )
+    assert (6, 6, 5) == helpers.convolution_output_dimension(
+        (6, 6), (3, 3), (1, 1), (1, 1), 5
+    )
+    assert (3, 3, 5) == helpers.convolution_output_dimension(
+        (6, 6), (3, 3), (2, 2), (1, 1), 5
+    )
+    assert (3, 1, 5) == helpers.convolution_output_dimension(
+        (6, 2), (3, 3), (2, 2), (1, 1), 5
+    )
+    assert (3, 1, 5) == helpers.convolution_output_dimension(
+        (6, 3), (3, 4), (2, 2), (1, 1), 5
+    )
+    assert (10, 4, 15) == helpers.convolution_output_dimension(
+        (12, 5), (3, 2), (1, 1), (0, 0), 15
+    )
+
+
+@hp.given(st.tuples(st.integers(1, 10), st.integers(1, 10)))
+def test_same_convolution_padding(filter_shape):
+    padding = helpers._same_convolution_padding(filter_shape)
+    assert (6, 6, 1) == helpers.convolution_output_dimension(
+        (6, 6), filter_shape, (1, 1), padding, 1
+    )
