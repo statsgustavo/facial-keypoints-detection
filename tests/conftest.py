9b943825eee9ea51db4d9c6bbce9cf07a5243760
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-24 13:59:42 -0300

    update: data fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import os
 
 import numpy as np
+import pandas as pd
 import pytest
 import tensorflow as tf
-from src.tools import data_loading, helpers
+from src.tools import data, helpers
 
 
 @pytest.fixture(scope="session")
@@ -12,20 +13,44 @@ def metadata():
 
 
 @pytest.fixture(scope="session")
-def load_params(metadata):
-    training_meta = metadata["training"]
-    path = os.path.join(training_meta["path"], training_meta["file"])
-    return path, metadata["delimiter"]
+def columns_names():
+    return [
+        "left_eye_center_x",
+        "left_eye_center_y",
+        "right_eye_center_x",
+        "right_eye_center_y",
+        "left_eye_inner_corner_x",
+        "left_eye_inner_corner_y",
+        "left_eye_outer_corner_x",
+        "left_eye_outer_corner_y",
+        "right_eye_inner_corner_x",
+        "right_eye_inner_corner_y",
+        "right_eye_outer_corner_x",
+        "right_eye_outer_corner_y",
+        "left_eyebrow_inner_end_x",
+        "left_eyebrow_inner_end_y",
+        "left_eyebrow_outer_end_x",
+        "left_eyebrow_outer_end_y",
+        "right_eyebrow_inner_end_x",
+        "right_eyebrow_inner_end_y",
+        "right_eyebrow_outer_end_x",
+        "right_eyebrow_outer_end_y",
+        "nose_tip_x",
+        "nose_tip_y",
+        "mouth_left_corner_x",
+        "mouth_left_corner_y",
+        "mouth_right_corner_x",
+        "mouth_right_corner_y",
+        "mouth_center_top_lip_x",
+        "mouth_center_top_lip_y",
+        "mouth_center_bottom_lip_x",
+        "mouth_center_bottom_lip_y",
+        "Image",
+    ]
 
 
 @pytest.fixture(scope="session")
-def one_image(load_params):
-    image, header = next(data_loading.read_raw_images(*load_params))
-    return image, header
-
-
-@pytest.fixture(scope="session")
-def parsed_feature_names():
+def features_names():
     return [
         "left_eye_center",
         "right_eye_center",
@@ -43,6 +68,25 @@ def parsed_feature_names():
         "mouth_center_top_lip",
         "mouth_center_bottom_lip",
     ]
+
+
+@pytest.fixture(scope="session")
+def dataset(columns_names):
+    nrows = 10
+    features = np.random.uniform(0, 96, (nrows, 30)).tolist()
+    images = np.random.randint(0, 256, (nrows, 96 * 96)).astype(str).tolist()
+
+    return pd.DataFrame(
+        [[*feat, " ".join(img)] for feat, img in zip(features, images)],
+        columns=columns_names,
+    )
+
+
+@pytest.fixture(scope="session")
+def dataset_csv_file(tmpdir_factory, dataset):
+    fixture_filename = str(tmpdir_factory.mktemp("data").join("dataset.csv"))
+    dataset.to_csv(fixture_filename, index=False)
+    return fixture_filename
 
 
 @pytest.fixture(scope="session")
