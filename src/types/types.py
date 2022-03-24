6b7459e15441967f8833f92b9e9e6b366c3bb3b8
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-24 13:59:17 -0300

    add: type aliases.

---
+++ src/types/types.py
@@ -0,0 +1,31 @@
+from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Type, Union
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# Data structure types
+ArrayType = np.ndarray
+
+TensorType = tf.Tensor
+
+DataframeType = pd.DataFrame
+
+DatasetType = tf.data.Dataset
+
+# Generica function argument types
+FnArgs = NewType("FnArgs", List[Any])
+
+FnKwargs = NewType("FnKwargs", Dict[str, Any])
+
+# Reader types
+RawDataReader = Callable[[str, Optional[FnKwargs]], DataframeType]
+
+# TransformerTypes
+FnImageConverterType = Callable[
+    [ArrayType, Optional[Union[FnArgs, FnKwargs]]], ArrayType
+]
+
+FnDataframeTransformerType = Callable[
+    [DataframeType, Optional[Union[FnArgs, FnKwargs]]], DataframeType
+]
