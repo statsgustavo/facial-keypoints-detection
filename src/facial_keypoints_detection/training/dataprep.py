06abae381892b4a2eaf8e04d806023584f54d31f
Luis Gustavo <stats.gustavo@gmail.com>
2022-03-25 17:45:32 -0300

    update: make function more general for preprocessing.

--- src/facial_keypoints_detection/training/dataprep.py
+++ src/facial_keypoints_detection/training/dataprep.py
@@ -2,62 +2,86 @@ import os
 from typing import NoReturn, Tuple
 
 import fire
+import toolz as tz
 from src.facial_keypoints_detection.tools import data, helpers
 from src.facial_keypoints_detection.types import DatasetType
 
 
-def _make_path(meta):
-    return os.path.join(meta["path"], meta["file"])
-
-
-def training_and_validation_split() -> NoReturn:
+def preprocess() -> NoReturn:
     """
-    Splits raw training dataset into training and validation files.
+    Executes data preprocessing steps. Fisrt, raw training and test datasets are loaded
+    from file; then missing values are filled for both dataset; after that, raw training
+    data is separated into training and validation. Finally, the processsed datasets are
+    stored in their homonymous interim folders.
     """
-    raw_meta = helpers.load_metadata(["data"], ["raw"])["data"]
-    interim_meta = helpers.load_metadata(["data"], ["interim"])["data"]
+    raw_data_params = helpers.load_dataset_params("raw")
+    interim_data_params = helpers.load_dataset_params("interim")
 
-    raw_training_data_path = _make_path(raw_meta["training"])
-
-    training_data_path = _make_path(interim_meta["training"])
-    validation_data_path = _make_path(interim_meta["validation"])
-
-    dataset = data.read_dataset_file(raw_training_data_path)
-    nrows = dataset.shape[0]
-
-    validation_dataset = dataset.sample(
-        frac=int(nrows * interim_meta["prop_validation"])
+    fn_pipe_training_validation_data = tz.compose_left(
+        data.read_dataset_file,
+        data.fill_null_values(fill_value=0.0),
+        data.training_and_validation_split(
+            validation_proportion=interim_data_params["prop_validation"],
+            seed=42,
+        ),
+        data.save_multiple_datasets(
+            file_paths=(
+                helpers.make_path(interim_data_params["training"]),
+                helpers.make_path(interim_data_params["validation"]),
+            ),
+            fn_save=data.save_dataset,
+        ),
     )
 
-    training_dataset = dataset.drop(validation_dataset.index, axis=0)
+    fn_pipe_test_data = tz.compose_left(
+        data.read_dataset_file,
+        data.fill_null_values(fill_value=0.0),
+        data.save_dataset(file_path=helpers.make_path(interim_data_params["test"])),
+    )
 
-    training_dataset.to_csv(training_data_path, index=False)
-    validation_dataset.to_csv(validation_data_path, index=False)
+    helpers.execute_in_multplipe_processes(
+        functions=[
+            fn_pipe_training_validation_data,
+            fn_pipe_test_data,
+        ],
+        fn_args=[
+            (helpers.make_path(raw_data_params["training"]),),
+            (helpers.make_path(raw_data_params["test"]),),
+        ],
+    )
 
 
 def load_and_parse_datasets(batch_size) -> Tuple[DatasetType, DatasetType, DatasetType]:
-    raw_meta = helpers.load_metadata(["data", "raw"])["data"]
-    interim_meta = helpers.load_metadata(["data", "interim"])["data"]
+    raw_meta = helpers.load_metadata(["data"], ["raw"])["data"]
+    interim_meta = helpers.load_metadata(["data"], ["interim"])["data"]
 
-    training_data = _make_path(interim_meta["training"])
-    validation_data = _make_path(interim_meta["validation"])
-    test_data = _make_path(raw_meta["test"])
+    training_data = helpers.make_path(interim_meta["training"])
+    validation_data = helpers.make_path(interim_meta["validation"])
+    test_data = helpers.make_path(raw_meta["test"])
 
     fn_load_and_parse = data.create_tensorflow_dataset(
-        fn_datset_reader=data.read_dataset_file, batch_size=batch_size
+        fn_dataset_reader=data.read_dataset_file,
+        batch_size=batch_size,
     )
 
     training, validation, test = (
-        fn_load_and_parse(training_data),
-        fn_load_and_parse(validation_data),
-        fn_load_and_parse(test_data),
+        fn_load_and_parse(path=training_data),
+        fn_load_and_parse(path=validation_data),
+        fn_load_and_parse(path=test_data),
     )
 
     return training, validation, test
 
 
 def cli():
-    fire.Fire({"split_dataset": training_and_validation_split})
+    """
+    Data processing routines.
+
+    :command preprocess: executes data preprocessing, which includes loading raw
+    dataset, handling of missing values, training-validation splitting and
+    writing processed datasets to disk.
+    """
+    fire.Fire({"preprocess": preprocess})
 
 
 if __name__ == "__main__":
